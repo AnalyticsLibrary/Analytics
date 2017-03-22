f67087cbd7483bb5b4011549c66f1094274383b6
Joris Joppe <joris@analys.io>
2017-03-22 12:01:15 +0100

    nieuwe analyses en wijziging in H001

---
+++ B_ANALYTICS/B1_ACL/A001.bat
@@ -0,0 +1,33 @@
+
+COMMENT
+***********************************************************
+analyse A001
+Mutaties per periode
+***********************************************************
+
+COM - eerst tabel generalLedger relateren aan transactions
+
+OPEN generalLedger
+INDEX ON accID TO "generalLedger_on_accID"
+OPEN transactions
+DEFINE RELATION accID WITH generalLedger INDEX generalLedger_on_accID
+
+COM - vervolgens veld aan maken bestaande uit periode en jaar zodat analyse
+COM - ook werkt met bestand waarin meerdere jaren zijn opgenomen
+COM - en bedrag-veld met het juiste teken
+
+DEFINE FIELD a_Period COMPUTED a_fiscalYear + "_" + ZONED(VAL(periodNumber, 0), 2)
+
+DEFINE FIELD a_Amount COMPUTED 
+
+	trLine_amnt * -1 IF ALLTRIM(trLine_amntTp) = "C"
+	trLine_amnt
+
+COM - vervolgens crosstabulate om output te genereren
+
+CROSSTAB ON accID generalLedger.accDesc COLUMNS a_Period SUBTOTAL a_Amount TO "A001.FIL" OPEN
+
+EXTRACT FIELDS ALL XLSX TO A001
+
+
+
--- B_ANALYTICS/B1_ACL/H001.bat
+++ B_ANALYTICS/B1_ACL/H001.bat
@@ -1,39 +1,51 @@
-COMMENT - uit header algemene informatie halen
+COMMENT
+***********************************************************
+analyse H001
+Mutaties per periode
+***********************************************************
 
-OPEN HEADER
-ASSIGN vBedrijf = companyName
-ASSIGN vJaar = fiscalYear
-
-COMMENT - transactions koppelen aan GeneralLedger om te bepalen wel RGS label per transctie is toegekend.
 
 OPEN transactions
 
-DEFINE FIELD bedrag COMPUTED 
+COM eerst velden aanmaken voor bedragen met de juiste tekens
+
+DEFINE FIELD a_Amount COMPUTED 
 
  trLine_amnt IF trLine_amntTp = "D"
  trLine_amnt * -1
 
-DEFINE FIELD btw_bedrag COMPUTED 
+DEFINE FIELD a_VAT_Amount COMPUTED 
 
  vatAmnt IF vatAmntTp = "D"
  vatAmnt * -1
 
-SORT periodNumber jrnTp custSupID  vatID IF MATCH(jrnTp "P" "S") AND (custSupID <> " ") AND (btw_bedrag <> 0.00) TO scr01 OPEN 
+COM vervolgens sorteren op relatie en alleen dagboeken inkoop en verkoop 
+COM bewaren 
+ 
+SORT a_fiscalYear a_companyName periodNumber jrnTp custSupID  vatID IF MATCH(jrnTp "P" "S") AND (custSupID <> " ") AND (a_VAT_Amount <> 0.00) TO scr01 OPEN 
+
+COM vervolgens relatie leggen tussen relaties en transacties om naam van relatie
+COM en landcode toe te kunnen voegen
 
 OPEN customersSuppliers
 INDEX ON custSupID TO "cs_on_csID"
 OPEN scr01
 DEFINE RELATION custSupID WITH customersSuppliers INDEX cs_on_csID
 
-SUMMARIZE ON periodNumber jrnTp custSupID customersSuppliers.custSupName AS 'custSupName' vatID OTHER customersSuppliers.taxRegistrationCountry AS 'taxRegistrationCountry' SUBTOTAL bedrag btw_bedrag  TO scr02 OPEN 
+COM vervolgens intellen op relatie
+
+SUMMARIZE ON periodNumber jrnTp custSupID customersSuppliers.custSupName AS 'custSupName' vatID OTHER a_fiscalYear a_companyName customersSuppliers.taxRegistrationCountry AS 'taxRegistrationCountry' SUBTOTAL a_Amount a_VAT_Amount  TO scr02 OPEN 
+
+COM effectief BTW percentage per relatie berekenenen. 
+COM als geen grondslag is opgenomen, dan default 100% opnemen.
 
 DEFINE FIELD percentage COMPUTED
 	
-	(btw_bedrag / bedrag) * 100.00 IF bedrag <> 0.00
-	bedrag
+	(a_VAT_Amount / a_Amount) * 100.00 IF a_Amount <> 0.00
+	100.00
 
+COM export genereren.  
  
-EXTRACT FIELDS SUBSTRING("%vBedrijf%" 1 50) AS 'bedrijf' SUBSTRING("%vJaar%" 1 4) AS 'boekjaar' periodNumber jrnTp custSupID custSupName taxRegistrationCountry bedrag btw_bedrag  percentage  TO A001 OPEN
+EXTRACT a_fiscalYear a_companyName periodNumber jrnTp custSupID custSupName taxRegistrationCountry a_Amount a_VAT_Amount percentage  TO H001 OPEN
 
-EXPORT FIELDS ALL XLSX TO A001
-
+EXPORT FIELDS ALL XLSX TO H001
