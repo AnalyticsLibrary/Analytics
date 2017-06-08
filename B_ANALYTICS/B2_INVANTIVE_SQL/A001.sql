cfbd3a6013aafd8b5d8c53e67a91b50c0c4d5a0e
Guido Leenders <guido.leenders@invantive.com>
2017-06-08 08:31:44 +0200

    Add Invantive SQL A001.sql

---
+++ B_ANALYTICS/B2_INVANTIVE_SQL/A001.sql
@@ -0,0 +1,21 @@
+﻿local remark Analyse A001
+
+local remark Saldibalans per periode
+
+select tle.interface_url
+,      tle.transaction_periodnumber
+,      gat.accid
+,      gat.accdesc
+,      sum(amnt) amnt
+from   transactionlines tle
+join   generalledgeraccounts gat
+on     gat.accid = tle.accid
+and    gat.interface_url = tle.interface_url
+group 
+by    tle.interface_url
+,     tle.transaction_periodnumber
+,     gat.accid
+,     gat.accdesc
+
+local export results as "c:\temp\A001.xlsx" format xlsx include headers include sql
+
