a82ddaf7db7105bc815af1fd2dbe624dce1867d8
Guido Leenders <guido.leenders@invantive.com>
2017-06-12 00:44:51 +0200

    Include cumulatives and opening balances in A001.

--- B_ANALYTICS/B2_INVANTIVE_SQL/A001.sql
+++ B_ANALYTICS/B2_INVANTIVE_SQL/A001.sql
@@ -2,20 +2,57 @@
 
 local remark Saldibalans per periode
 
-select tle.interface_url
-,      tle.transaction_periodnumber
-,      gat.accid
-,      gat.accdesc
-,      sum(amnt) amnt
-from   transactionlines tle
-join   generalledgeraccounts gat
-on     gat.accid = tle.accid
-and    gat.interface_url = tle.interface_url
-group 
-by    tle.interface_url
-,     tle.transaction_periodnumber
-,     gat.accid
-,     gat.accdesc
+select dtl.interface_url
+,      dtl.company_name
+,      dtl.fiscal_year_number
+,      dtl.transaction_periodnumber
+,      dtl.accid
+,      dtl.accdesc
+,      dtl.opening_balance
+,      dtl.balance
+,      coalesce(dtl.opening_balance, 0) + coalesce(dtl.balance, 0) total_balance
+from   ( select gat.interface_url
+         ,      gat.company_name
+         ,      gat.fiscal_year_number
+         ,      prd.periodnumber transaction_periodnumber
+         ,      gat.accid
+         ,      gat.accdesc
+         ,      obe.balance opening_balance
+         ,      sum(tle.balance) balance
+         from   generalledgeraccounts gat
+         join   periods prd
+         on     prd.interface_url = gat.interface_url
+         left 
+         outer
+         join   transactionlines tle
+         on     tle.accid                    = gat.accid
+         and    tle.interface_url            = gat.interface_url
+         and    tle.transaction_periodnumber <= prd.periodnumber
+         left 
+         outer 
+         join   OpeningBalanceLines obe
+         on     obe.interface_url = gat.interface_url
+         and    obe.accid         = gat.accid
+         group 
+         by     gat.interface_url
+         ,      gat.company_name
+         ,      gat.fiscal_year_number
+         ,      gat.accid
+         ,      gat.accdesc
+         ,      prd.periodnumber
+         ,      obe.balance
+       ) dtl
+where  coalesce(dtl.opening_balance, 0) != 0
+       or
+       coalesce(dtl.balance, 0) != 0
+order 
+by     dtl.interface_url
+,      dtl.company_name
+,      dtl.fiscal_year_number
+,      dtl.transaction_periodnumber
+,      dtl.accid
+,      dtl.accdesc
+
+select * from generalledgeraccounts
 
 local export results as "c:\temp\A001.xlsx" format xlsx include headers include sql
-
